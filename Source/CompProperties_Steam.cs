66010155a20b7106c0e75188a55aa6aed18e248a
fyarn <1361631+fyarn@users.noreply.github.com>
2017-08-27 15:11:44 -0700

    Created project, added CompProperties for steam

---
+++ Source/CompProperties_Steam.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace SteamCorp
+{
+    public class CompProperties_Steam : CompProperties
+    {
+        public bool transmitsPower;
+
+        public float basePowerConsumption;
+
+        public SoundDef soundPowerOn;
+
+        public SoundDef soundPowerOff;
+
+        public SoundDef soundAmbientPowered;
+    }
+}
