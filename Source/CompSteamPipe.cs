9e9444f1c2ea8f54392f334295f23e9ce7bdcad1
fyarn <1361631+fyarn@users.noreply.github.com>
2017-08-27 18:59:00 -0700

    - edited the power defs to reference planned classes - empty pipe building class to start - added SteamPipe class with gridID

---
+++ Source/CompSteamPipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace SteamCorp
+{
+    class CompSteamPipe : ThingComp
+    {
+        private int intGridID;
+
+        public int GridID
+        {
+            get => intGridID;
+            set => intGridID = value;
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+        }
+    }
+}
